fcf290ae9dc34b81d8acabed483469b03a320780
York <yorkzhang520@gmail.com>
2020-01-05 15:15:08 +0800

    change: update the instructions of CLI commands

--- lib/flr.rb
+++ lib/flr.rb
@@ -15,37 +15,37 @@ module Flr
       Command.version
     end
 
-    desc "init", "Generate `Flrfile.yaml` and auto specify package `r_dart_library` in `pubspec.yaml`"
+    desc "init", "Add flr configuration and dependency \"r_dart_library\" into pubspec.yaml"
     long_desc <<-LONGDESC
-      Create a `Flrfile.yaml` file for the current directory if none currently exists,
-      and automatically specify package `r_dart_library`(https://github.com/YK-Unit/r_dart_library) in `pubspec.yaml`.
+      Add flr configuration 
+      and dependency \"r_dart_library\"(https://github.com/YK-Unit/r_dart_library) into pubspec.yaml.
 
     LONGDESC
     def init
       Command.init
     end
 
-    desc "generate", "Perform once a assets scan, then auto specify scanned assets in `pubspec.yaml`, and generate `R.dart`"
+    desc "generate", "Perform once a assets scan, then auto specify scanned assets in pubspec.yaml, and generate \"r.g.dart\""
     long_desc <<-LONGDESC
       Perform once a assets scan for your project,
-      then automatically specify scanned assets in `pubspec.yaml`,
-      and generate `R.dart` file.
+      then automatically specify scanned assets in pubspec.yaml,
+      and generate "r.g.dart" file.
 
     LONGDESC
     def generate
       Command.generate
     end
 
-    desc "monitor", "Launch a monitoring service. When detects asset changes, the service will auto execute `flr generate`. Press `Ctrl-C` can terminate it"
+    desc "monitor", "Launch a monitoring service. When detects asset changes, the service will auto execute \"flr generate\". Press \"Ctrl-C\" can terminate it"
     long_desc <<-LONGDESC
        Launch a monitoring service that continuously monitors asset changes for your project. 
 
-       If there are any changes, it will automatically execute `flr generate` command which 
+       If there are any changes, it will automatically execute "flr generate" command which 
        will perform a assets scan,
-       then automatically specify scanned assets in `pubspec.yaml`,
-       and generate the `R.dart` file.
+       then automatically specify scanned assets in pubspec.yaml,
+       and generate the "r.g.dart" file.
 
-       You can terminate the service by manually pressing `Ctrl-C`.
+       You can terminate the service by manually pressing "Ctrl-C".
 
     LONGDESC
     def monitor
