ffe5407aceff51d2560165e6e7e2112300b47cf7
York <yorkzhang520@gmail.com>
2021-03-21 17:18:19 +0800

    feat(code_util): 新增should_support_nullsafety方法

--- lib/flr/util/code_util.rb
+++ lib/flr/util/code_util.rb
@@ -5,6 +5,36 @@ module Flr
   # 代码生成相关的工具类方法
   class CodeUtil
 
+    @@should_support_nullsafety = nil
+
+    def self.should_support_nullsafety
+      if @@should_support_nullsafety != nil
+        return @@should_support_nullsafety
+      end
+
+      # $ flutter --version
+      # Flutter 1.12.13+hotfix.5 • channel stable • https://github.com/flutter/flutter.git
+      # Framework • revision 27321ebbad (5 weeks ago) • 2019-12-10 18:15:01 -0800
+      # Engine • revision 2994f7e1e6
+      # Tools • Dart 2.7.0
+      flutter_version_result = `flutter --version`
+      if (flutter_version_result.nil? == true || flutter_version_result.empty? == true)
+        @@should_support_nullsafety = false
+        return @@should_support_nullsafety
+      end
+
+      dart_version_str = flutter_version_result.split("Dart")[1]
+      dart_version_str.strip!
+
+      if Version.new(dart_version_str) >= Version.new("2.12.0")
+        @@should_support_nullsafety = true
+        return @@should_support_nullsafety
+      end
+
+      @@should_support_nullsafety = false
+      return @@should_support_nullsafety
+    end
+
     # generate_R_class(package_name) -> string
     #
     # 根据模板生成 R class 的代码
